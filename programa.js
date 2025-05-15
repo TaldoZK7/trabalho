06a2aeb98a755a09d3bc1c1a82bbd41cea317c58
TaldoZK7 <zackminews@gmail.com>
2025-05-15 14:12:59 -0400

    Add files via upload

---
+++ programa.js
@@ -0,0 +1,5 @@
+let botao = document.querySelector("#botao")
+botao.style.background="yellow";
+botao.addEventListener("click",e =>{
+    botao.innerHTML="Saudações Tutora, venha me conhecer melhor"
+});
